2c2f0448d038184c1f5096fd8378feaff1b8d5a6
Florian Sauer <sauer.florian@gmx.de>
2022-08-01 16:28:30 +0200

    fix for changed timestamp format of packt api

--- src/config.py
+++ src/config.py
@@ -22,4 +22,4 @@ URL_BOOK_TYPES_ENDPOINT = "products-v1/products/{book_id}/types"
 URL_BOOK_ENDPOINT = "products-v1/products/{book_id}/files/{format}"
 
 # timestamp format used by packtpub api
-TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+TIMESTAMP_FORMAT = "%Y-%m-%d %H:%M:%S"
